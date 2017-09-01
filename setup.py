8e06cca084c82f094cdaecbdcb67cd2a9d313fcf
Michael Goddard <mgoddard@gopivotal.com>
2017-09-01 11:04:54 -0400

    Correcting typo in package name

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-  name='pcfongcp',
+  name='pcfgcp',
   version='0.1.0',
   description='Library to facilitate consuming Google Cloud Platform services',
   url='https://github.com/pivotal-alliances-immersion/pcf-gcp-python',
   author='Michael Goddard',
   author_email='mgoddard@pivotal.io',
   license='LICENSE.txt',
-  packages=['pcfongcp'],
+  packages=['pcfgcp'],
   install_requires=[
     "google-auth == 1.0.2",
   ],
